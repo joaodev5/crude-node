310d3f251d7d498a75026273aa87eadf80843387
ArielleSpector <138306922+ArielleSpector@users.noreply.github.com>
2023-07-22 11:30:32 -0300

    implementação do cadastro de carros

--- src/routes/creat-car.js
+++ src/routes/creat-car.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const database = require('../database')
+
 
 const route = express()
 
-route.post('/car', () => {
+route.post('/car', (req, res) => {
+    const car = req.body
+    
+    database.push(car)
 
+    return res.status(201).json(car)
 })
 
 module.exports = route
